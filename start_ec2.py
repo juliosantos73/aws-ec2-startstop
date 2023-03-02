11fc4f5391268ed09309aa966f9ff03d7e480d4b
Júlio César Santos <juliosantos73@gmail.com>
2023-03-02 05:48:14 -0400

    start method

--- start_ec2.py
+++ start_ec2.py
@@ -9,9 +9,15 @@ def is_running(instance):
         is_running = True
     return is_running
 
+def is_dev(instance):
+    tag = {'Key':'ScheduledStartStop', 'Value':'true'}
+    is_dev = False
+    if tag in instance['Tags']:
+        is_dev = True
+    return is_dev
+
 def start_ec2_instances():
     start_time = datetime.now()
-
 
 def lambda_handler(event, context):
     print("Checking instances...")
